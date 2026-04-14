e29628537efdbac1a3d25f69d64ff1a35923551b
Leonardo Moraes <demoraesleonardo327@gmail.com>
2026-04-14 19:28:51 -0300

    main com comentarios

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,13 +5,17 @@ import javafx.stage.Stage;
 import view.LoginUsuario;
 
 public class Main extends Application {
-
+	/**
+	 * Classe Principal (Main) da Aplicação 
+	 */
     @Override
     public void start(Stage stage) {
+    	// abre tela de login
         new LoginUsuario().start(stage); 
     }
 
     public static void main(String[] args) {
+    	 // launch() inicia a aplicação JavaFX e chama o método start()
         launch();
      
     }
